5903233c24e229955f4e972834342afcfa0a2eff
creditcoder <creditcoder@hotmail.com>
2020-03-23 04:49:46 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Admin, Resource } from "react-admin";
+import { TodoList, TodoEdit, TodoCreate } from "./todos";
+import { UserList, UserShow } from "./users";
+import buildHasuraProvider from "ra-data-hasura-graphql";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+
+// const uri = "https://low-code-api.herokuapp.com/v1/graphql";
+const uri = "https://fitness-service.herokuapp.com/v1/graphql";
+
+const App = async () => {
+  const hasuraDataProvider = await buildHasuraProvider({
+    clientOptions: { uri: uri }
+  });
+
+  return () => (
+    <Admin
+      dataProvider={hasuraDataProvider}
+      // authProvider={authProvider}
+      // dashboard={Dashboard}
+    >
+      <Resource
+        name="todos"
+        icon={PostIcon}
+        list={TodoList}
+        edit={TodoEdit}
+        create={TodoCreate}
+      />
+      <Resource name="users" icon={UserIcon} list={UserList} show={UserShow} />
+    </Admin>
+  );
+};
+
+export default App;
